362ab0e7a1a917b1bcf17a33e034eb57e056319b
Tirso Pascual <tpascualgarcia@gmail.com>
2022-07-21 09:59:39 +0900

    login form ongoing

--- src/pages/login/Login.js
+++ src/pages/login/Login.js
@@ -1,9 +1,29 @@
+import { useState } from 'react'
+
 // styles
 import styles from './Login.module.css'
 
 
 export default function Login() {
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+
   return (
-    <div>Login</div>
+    <form className={styles['login-form']}>
+      <h2>Login</h2>
+
+      <label htmlFor="email">Email:</label>
+      <input type="text" id='email'
+        onChange={(e) => setEmail(e.target.value)}
+        value={email}
+      // we set the email to whatever the user is typing by onChange. Also we set a value just in case we change this field from outside the input, so the input reflect that change too. So we have a 2 way binding and this is called a controlled input
+      />
+
+      <label htmlFor="password">Password:</label>
+      <input type="password" id='password'
+      />
+
+      <button className="btn">Login</button>
+    </form>
   )
 }
