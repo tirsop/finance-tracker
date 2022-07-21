43a03fa2bddd5fb6162cb56f16d5edce163c9080
Tirso Pascual <tpascualgarcia@gmail.com>
2022-07-21 16:41:50 +0900

    setup firebase

---
+++ src/firebase/config.js
@@ -0,0 +1,21 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDjEn68-p3J5Mh6UGX28PiVzt_9NG7Hksw",
+  authDomain: "finance-tracker-e5627.firebaseapp.com",
+  projectId: "finance-tracker-e5627",
+  storageBucket: "finance-tracker-e5627.appspot.com",
+  messagingSenderId: "741180877481",
+  appId: "1:741180877481:web:1ae5dfdf042344ab6b97b8"
+};
+
+// Initialize Firebase
+initializeApp(firebaseConfig);
+
+// Initialize services
+const db = getFirestore()
+
+export { db }
