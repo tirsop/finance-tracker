6079d0853e1d4ac9e5eb4176283a6e19b94f51d8
Tirso Pascual <tpascualgarcia@gmail.com>
2022-07-22 16:11:11 +0900

    wrap App in authContext for accessing the user everywhere

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -11,7 +11,7 @@ export const authReducer = (state, action) => {
   }
 }
 
-export const AuthContextProvider = ({ children }) => {
+export const AuthtProvider = ({ children }) => {
 
   const [state, dispatch] = useReducer(authReducer, {
     user: null
