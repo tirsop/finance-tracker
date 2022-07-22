7578223782d0852092451386f9c0b7eadd9c7d6c
Tirso Pascual <tpascualgarcia@gmail.com>
2022-07-22 16:42:29 +0900

    logout functionality

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,15 +1,22 @@
+import { useContext } from 'react'
 import { Link } from 'react-router-dom'
+import { AuthContext } from '../context/AuthContext'
+import { useLogout } from '../hooks/useLogout'
 // styles
 import styles from './Navbar.module.css'
 
 
 export default function Navbar() {
+  const { user } = useContext(AuthContext)
+  const { logout } = useLogout()
+
   return (
     <nav className={styles.navbar}>
       <ul>
         <li className={styles.title}>myMoney</li>
-        <li><Link to="/login">Login</Link></li>
-        <li><Link to="/signup">Signup</Link></li>
+        {!user && <li><Link to="/login">Login</Link></li>}
+        {!user && <li><Link to="/signup">Signup</Link></li>}
+        {user && <li><button className="btn" onClick={logout}>Logout</button></li>}
       </ul>
     </nav>
   )
