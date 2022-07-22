b5637f3b3b458c21ee02d552c7e84f8164041161
Tirso Pascual <tpascualgarcia@gmail.com>
2022-07-22 15:53:22 +0900

    auth context setting

---
+++ src/context/AuthContext.js
@@ -0,0 +1,13 @@
+import { createContext } from "react";
+
+
+export const AuthContext = createContext()
+
+export const AuthContextProvider = ({ children }) => {
+
+  return (
+    <AuthContextProvider>
+      {children}
+    </AuthContextProvider>
+  )
+}
