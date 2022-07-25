46b76e04d739aaef0278e6c56b530c012282a947
Tirso Pascual <tpascualgarcia@gmail.com>
2022-07-25 20:20:01 +0900

    add spinner for loading state

---
+++ src/components/Spinner.jsx
@@ -0,0 +1,9 @@
+// styles
+import './Spinner.css'
+
+export default function Spinner() {
+  return (
+    <div className="lds-ring"><div></div><div></div><div></div><div></div></div>
+  )
+}
+
--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -4,6 +4,7 @@ import { useCollection } from '../../hooks/useCollection'
 // components
 import TransactionForm from './TransactionForm'
 import TransactionList from './TransactionList'
+import Spinner from '../../components/Spinner'
 // styles
 import styles from './Home.module.css'
 
@@ -16,7 +17,7 @@ export default function Home() {
     <div className={styles.container}>
       <div className={styles.content}>
         {error && <p>{error}</p>}
-        {isPending && <p>Loading...</p>}
+        {isPending && <Spinner />}
         {data && <TransactionList transactions={data} />}
       </div>
       <div className={styles.sidebar}>
