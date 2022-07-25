9010cf122153494c0e520dfd727e71a181825cfd
Tirso Pascual <tpascualgarcia@gmail.com>
2022-07-25 19:22:58 +0900

    create TransactionList component inside Home

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,18 +1,23 @@
 import { AuthContext } from '../../context/AuthContext'
 import { useContext } from 'react'
+import { useCollection } from '../../hooks/useCollection'
 // components
 import TransactionForm from './TransactionForm'
+import TransactionList from './TransactionList'
 // styles
 import styles from './Home.module.css'
 
 
 export default function Home() {
   const { user } = useContext(AuthContext)
+  const { data, isPending, error } = useCollection()
 
   return (
     <div className={styles.container}>
       <div className={styles.content}>
-
+        {error && <p>{error}</p>}
+        {isPending && <p>Loading...</p>}
+        {data && <TransactionList transacions={data} />}
       </div>
       <div className={styles.sidebar}>
         <TransactionForm uid={user.uid} />
