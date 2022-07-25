3946eb56aebf3a89f07d1b5e632bb5e1e62d86aa
Tirso Pascual <tpascualgarcia@gmail.com>
2022-07-25 16:01:27 +0900

    coding useFirestore hook

--- src/hooks/useFireStore.js
+++ src/hooks/useFireStore.js
@@ -1,5 +1,7 @@
 import { useReducer, useEffect, useState } from 'react'
 import { db } from '../firebase/config'
+import { addDoc } from 'firebase/firestore'
+
 
 let initialState = {
   document: null,
@@ -10,6 +12,12 @@ let initialState = {
 
 const firestoreReducer = (state, action) => {
   switch (action.type) {
+    case 'IS_PENDING':
+      return { document: null, isPending: true, error: null, success: false }
+    case 'ADDED_DOCUMENT':
+      return { document: action.payload, isPending: false, error: null, success: true }
+    case 'ERROR':
+      return { document: null, isPending: false, error: action.payload, success: false }
 
     default:
       return state
@@ -20,18 +28,33 @@ export const useFireStore = (collection) => {
   const [response, dispatch] = useReducer(firestoreReducer, initialState)
   const [isCancelled, setIsCancelled] = useState(false)
 
-  const ref = db.collection(collection)
+  const ref = collection(db, collection)
 
-  const addDocument = (doc) => {
-
+  // only dispatch (update state) if request is not cancelled (go to another page while loading)
+  const dispatchIfNotCancelled = (action) => {
+    if (!isCancelled) {
+      dispatch(action)
+    }
   }
-  const deleteDocument = (id) => {
+
+  const addDocument = async (doc) => {
+    dispatch({ type: 'IS_PENDING' })
+    try {
+      const addedDocument = await addDoc(ref, doc)
+      dispatchIfNotCancelled({ type: 'ADDED_DOCUMENT', payload: addedDocument })
+    } catch (err) {
+      dispatchIfNotCancelled({ type: 'ERROR', payload: err.message })
+    }
+  }
+
+  const deleteDocument = async (id) => {
 
   }
 
   useEffect(() => {
     return () => setIsCancelled(true)
   }, [])
+
 
   return { addDocument, deleteDocument, response }
 }
