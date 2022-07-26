943c408aa4a56e461448286af905f33516f6f45d
Tirso Pascual <tpascualgarcia@gmail.com>
2022-07-26 11:40:21 +0900

    adjust spinner position

--- src/components/Spinner.jsx
+++ src/components/Spinner.jsx
@@ -3,7 +3,14 @@ import './Spinner.css'
 
 export default function Spinner() {
   return (
-    <div className="lds-ring"><div></div><div></div><div></div><div></div></div>
+    <div className="lds-ring-flex">
+      <div className="lds-ring">
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+      </div>
+    </div>
   )
 }
 
